1dcf8a8601aa27733955a11c7e16bcf773a5bb04
Iconvibes <ferdinardoluwajuwonlo@gmail.com>
2026-08-29 03:40:34 +0100

    rebuild vite to /root

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: resolve(__dirname, '..', 'dist'),
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
     proxy: {
